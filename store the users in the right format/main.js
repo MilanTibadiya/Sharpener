1c399b3186187bcf20d52d3d3652570df52d736f
MilanTibadiya <tibadiyamilan@gmail.com>
2022-12-21 12:07:36 +0530

    Task-11

---
+++ store the users in the right format/main.js
@@ -0,0 +1,34 @@
+
+
+// Put DOM elements into variables
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+
+
+// Listen for form submit
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  
+  if(nameInput.value === '' || emailInput.value === '') {
+    // alert('Please enter all fields');
+    msg.classList.add('error');
+    msg.innerHTML = 'Please enter all fields';
+
+    // Remove error after 3 seconds
+    setTimeout(() => msg.remove(), 3000);
+  } else { 
+       // store user name in local storage   &&&&& EVERY TIME KEY,VALUE PAIR IN KEY DIIFT SO ITS STORE IN IT DIGFF
+   localStorage.setItem(nameInput.value,emailInput.value );
+
+    // Add text node with input values
+   console.log(`${nameInput.value}: ${emailInput.value}`);
+
+    // Clear fields
+    nameInput.value = '';
+    emailInput.value = '';
+  }
+}
